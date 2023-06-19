196a21d9143e288df586fa8d50de2d346272e356
Jyoti Ranjan Giri <jyotiranjangiri.jsr@gmail.com>
2023-06-19 18:45:11 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+<div class="hero">
+    <div class="container">
+         <h1>
+            some really interesting text
+         </h1>
+         <p>
+            We use the same configuration as Parcel to bundle this sandbox, you can find more info about Parcel.
+         </p>
+    </div>
+</div>
+`;
